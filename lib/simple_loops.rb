86481c1c5c9b2f9766849a88290f48844c19bcf5
Bill Feng <bill.feng@yale.edu>
2021-05-23 00:09:22 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,4 +1,5 @@
 # Write your methods here
+require 'pry'
 def loop_message_five_times(message)
   5.times do
     puts message
@@ -22,6 +23,8 @@ def return_string_array(array)
   for element in array do
     newArray.push[element.to_s]
   end
+  binding.pry
   return newArray
 end
 
+p return_string_array([1, 2, 3, 4, 5])
