aea3e3f6d6544909ac0181485605f2632d981546
Bill Feng <bill.feng@yale.edu>
2021-05-23 00:09:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,27 @@
 # Write your methods here
+def loop_message_five_times(message)
+  5.times do
+    puts message
+  end
+end
+
+def loop_message_n_times(message, n)
+  n.times do
+    puts message
+  end
+end
+
+def output_array(array)
+  for str in array do
+    puts str
+  end
+end
+
+def return_string_array(array)
+  newArray=[]
+  for element in array do
+    newArray.push[element.to_s]
+  end
+  return newArray
+end
+
